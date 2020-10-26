3d95aaac3df8251cc46d3034e77f66d663173745
septbr <septr@qq.com>
2020-10-26 16:46:23 +0800

    fixed

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-call dotnet publish -c release --self-contained=false -r win10-x64 /p:PublishSingleFile=true
+call dotnet publish -c Release -r win-x64 --self-contained=false /p:PublishSingleFile=true
 
 if errorlevel 0 (echo successful.) else (echo failed.)
 pause > nul
