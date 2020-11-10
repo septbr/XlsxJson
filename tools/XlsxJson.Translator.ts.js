8d7115489325a4fb73cab723ffe84675cc683452
septbr <septr@qq.com>
2020-11-10 16:01:50 +0800

    fixed bug

--- tools/XlsxJson.Translator.ts.js
+++ tools/XlsxJson.Translator.ts.js
@@ -7,6 +7,8 @@ let tsTemplate = `export namespace xlsx {
     export type i16 = number;
     export type i32 = number;
     export type i64 = number;
+    export type f32 = number;
+    export type f64 = number;
     export type bool = boolean;
     export type str = string;
 
